68732221febdfc1dcf7f042b8cce69ecb212abba
Mika1212 <ilravgaz03@mail.ru>
2024-10-18 12:39:09 +0300

    Revert "?"
    This reverts commit 2c1c210dab2aa0ec12130cd4aeb598f85c986b5b.
    
    wrong

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,6 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  # Activates an account.
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end 
-
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -47,10 +36,9 @@ class User < ApplicationRecord
 
 
   # Returns true if the given token matches the digest.
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
