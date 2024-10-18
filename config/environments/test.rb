ec5b6adc57c7e49e3b388ba7e1a7b7002f8175f4
Mika1212 <ilravgaz03@mail.ru>
2024-10-18 11:01:23 +0300

    Mid-way

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'gmail.com' }
+
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
